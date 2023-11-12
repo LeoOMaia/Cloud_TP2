40a50310ae2c888a819a2138a2871f2b76d28b89
Pephma <103604628+Pephma@users.noreply.github.com>
2023-11-11 21:12:10 -0300

    Add files via upload

--- Rest_API/client.py
+++ Rest_API/client.py
@@ -1,3 +1,18 @@
 import requests
-import json
 import sys
+import json
+
+api_url = 'http://127.0.0.1:5000/api/recommend'
+
+input_playlist = sys.argv[1:]
+
+data = json.dumps({'songs': input_playlist})
+
+headers = {'Content-Type': 'application/json'}
+
+response = requests.post(api_url, data=data, headers=headers)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Error: {response.status_code}")
--- Rest_API/server.py
+++ Rest_API/server.py
@@ -1,3 +1,24 @@
 from flask import Flask, request, jsonify
 import pickle
-import pandas as pd
+
+app = Flask(__name__)
+
+rules_path = 'rules.pkl'
+
+@app.route('/api/recommend', methods=['POST'])
+def recommend():
+    songs = request.json['songs']
+
+    rules = pickle.load(open(rules_path, 'rb'))
+
+    recommended_songs = []
+
+    for song in songs:
+        recommended_songs.append(rules[rules['antecedents'] == {song}]['consequents'].values[0])
+
+    return jsonify(recommended_songs)
+
+if __name__ == '__main__':
+    app.run(port=5000)
+
+
