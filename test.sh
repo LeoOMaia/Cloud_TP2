d3794f6d94cb5a5a54ba3596967c127c49cd19d3
LeoOMaia <leonardooliveiramaia1@gmail.com>
2023-11-26 17:36:33 +0000

    update

--- test.sh
+++
@@ -1,5 +0,0 @@
-wget --server-response \
-     --output-document response.out \
-     --header='Content-Type: application/json' \
-     --post-data '{"songs": ["Yesterday", "Bohemian Rhapsody"]}' \
-     http://10.111.233.250:32171/api/recommend
